ee9d4c5560f70544036adfcc2fae1cbae43a7f41
Sam Saint-Pettersen <s.stpettersen@gmail.com>
2013-04-13 00:17:34 +0100

    -jQuery in Node and tidy up hgroutes.js.

--- routes/hgroutes.js
+++ routes/hgroutes.js
@@ -7,8 +7,7 @@ Released under the MIT/X11 License.
 */
 'use strict';
 
-var hglib = require('../lib/hglib.js')
-
+var hglib = require('../lib/hglib.js');
 module.exports = {
 	getIndex: function(req, res) {
 		hglib.createCookie(res);
